f37076ebd6d58e9e697c0f3b34425b3d2091842d
dqmdz <daniel.quinterospinto@gmail.com>
2024-07-17 10:25:52 -0300

    Configuring Eureka Client

--- src/main/java/pys/api/rest/configuration/CoreConfiguration.java
+++ src/main/java/pys/api/rest/configuration/CoreConfiguration.java
@@ -1,12 +1,14 @@
 package pys.api.rest.configuration;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
+@EnableDiscoveryClient
 @PropertySource("classpath:config/factura.properties")
-public class SpringConfiguration {
+public class CoreConfiguration {
 
 }
